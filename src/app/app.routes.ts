8bb53cea9991fd7fc23408fe831ca075ea8810b0
AhmedM7med <ahmedelgebaly2511@gmail.com>
2025-08-13 10:36:54 +0300

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home-component/home-component';
+import { AboutComponent } from '../components/about-component/about-component';
+import { LatestComponent } from '../components/latest-component/latest-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'latest', component: LatestComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/components/latest-component/latest-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-component',
+  imports: [],
+  templateUrl: './latest-component.html',
+  styleUrl: './latest-component.css'
+})
+export class LatestComponent {
+
+}
